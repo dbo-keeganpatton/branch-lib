70a6807485799212171ad5880cfd8667a9710efd
eyelady <keeganpatton@gmail.com>
2025-11-25 14:24:49 -0600

    Fix Banner style | update images

--- branch-lib/components/topBanner.tsx
+++ branch-lib/components/topBanner.tsx
@@ -2,42 +2,52 @@ import Image from "next/image"
 
 export default function TopBanner() {
 
-  const inboxImage = "/inbox-message-mail-svgrepo-com.svg"
+  const inboxImage = "/mail-svgrepo-com.svg"
   const friendList = "/friends-svgrepo-com.svg"
-  const homeImage = "/home-svgrepo-com.svg"
+  const homeImage = "/home-icon-svgrepo-com.svg"
   const menuImage = "/menu-symbol-svgrepo-com.svg"
 
+  const userName = "TODO"
+
   return (
-    <div className="flex justify-between items-center w-full p-5 bg-purple-400/10">
-      <Image
-        src={menuImage}
-        width={25}
-        height={25}
-        alt="Button for menu expander"
-      />
+    <div className="flex justify-between items-center w-full/2 p-5 m-2 bg-purple-400/10 rounded-sm">
 
-      <Image
-        src={homeImage}
-        width={25}
-        height={25}
-        alt="Button for menu expander"
-      />
+      <div>
+        <Image
+          src={menuImage}
+          width={25}
+          height={25}
+          alt="Button for menu expander"
+        />
+      </div>
 
-      <Image
-        src={friendList}
-        width={25}
-        height={25}
-        alt="Button for menu expander"
-      />
+      {/* TODO: Add username auth personal greeting flow */}
+      <div>Hello again {userName}</div>
+      {/* END TODO */}
 
-      <Image
-        src={inboxImage}
-        width={25}
-        height={25}
-        alt="Button for menu expander"
-      />
+      <div className="flex justify-end gap-3">
+        <Image
+          src={homeImage}
+          width={25}
+          height={25}
+          alt="Button for menu expander"
+        />
 
+        <Image
+          src={inboxImage}
+          width={25}
+          height={25}
+          alt="Button for menu expander"
+        />
 
+        <Image
+          src={friendList}
+          width={25}
+          height={25}
+          alt="Button for menu expander"
+        />
+
+      </div>
 
     </div>
   );
