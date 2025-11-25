995b8a985640253ccf020abec940f698d42d4522
eyelady <keeganpatton@gmail.com>
2025-11-25 13:42:06 -0600

    working db api

---
+++ branch-lib/components/QueryDatabase.tsx
@@ -0,0 +1,34 @@
+import { Pool } from "pg";
+
+export default async function QueryDatabase() {
+
+  const pool = new Pool({ connectionString: process.env.POSTGRES_URL });
+  const results = await pool.query('select * from branch_lib.branch_lib_core.dim_author_story limit 10;');
+  const results_array = results.rows
+
+  return (
+    <div>
+
+      <ul className="space-y-4">
+
+        {/* Inject the result set wherever from DB */}
+        {results_array.rows.map((result) => (
+
+          <li key={result.title}>
+            <div>
+              <p>{result.author_id}</p>
+              <p>{result.story_id}</p>
+            </div>
+            <span>{result.created_timestamp.toISOString()}</span>
+          </li>
+
+        ))}
+
+      </ul>
+
+    </div>
+  );
+
+};
+
+
