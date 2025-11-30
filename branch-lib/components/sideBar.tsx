f666f551c4cbd304366ff7f87ec6af89d6211b31
eyelady <keeganpatton@gmail.com>
2025-11-30 14:00:21 -0600

    Sidebar Animation | Fckd layout

--- branch-lib/components/sideBar.tsx
+++ branch-lib/components/sideBar.tsx
@@ -9,21 +9,21 @@ function SideBar({ open }: { open: boolean }) {
     <div
       className={`
         flex-row bg-purple-400/10 rounded-sm p-2
-        transition-transform duration-300
+        transition-transform duration-200 h-full 
         ${open ? "translate-x-0" : "-translate-x-full"}
       `}
     >
-      <h1 className="text-md text-purple-500/50"> What&apos;s New? </h1>
+      <h1 className="text-sm text-purple-500/50"> What&apos;s New? </h1>
       <br />
 
-      <h1 className="text-md text-purple-500/50"> Top Categories </h1>
-      <ul className="border-l border-l-gray-600/50 ml-2 p-2 w-30">
-        <li className="mt-2 p-2 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Science Fiction </li>
-        <li className="mt-2 p-2 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Biographies </li>
-        <li className="mt-2 p-2 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Agriculture </li>
-        <li className="mt-2 p-2 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Literary Fiction </li>
-        <li className="mt-2 p-2 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Fantasy </li>
-        <li className="mt-2 p-2 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Horror </li>
+      <h1 className="text-sm text-purple-500/50">  Categories </h1>
+      <ul className="border-l border-l-gray-600/50 pl-2">
+        <li className="mt-1 p-1 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Science Fiction </li>
+        <li className="mt-1 p-1 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Biographies </li>
+        <li className="mt-1 p-1 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Agriculture </li>
+        <li className="mt-1 p-1 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Literary Fiction </li>
+        <li className="mt-1 p-1 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Fantasy </li>
+        <li className="mt-1 p-1 text-xs border border-purple-400/5 hover:border-purple-400/50 rounded-xs bg-purple-400/2"> Horror </li>
       </ul>
 
     </div>
@@ -40,7 +40,7 @@ export default function SidebarClient({ children }: { children: React.ReactNode 
       <TopBanner onMenuClick={() => setOpen(!open)} />
 
       <div className="flex pl-2 pr-2">
-        <SideBar open={open} />
+        <div className="overflow-hidden w-md"> <SideBar open={open} /> </div>
         {children}
       </div>
     </>
