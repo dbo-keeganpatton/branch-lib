db077af4ecd56bcac67c453d7725fda0fa5da8c7
eyelady <keeganpatton@gmail.com>
2025-12-05 21:47:03 -0600

    appease compiler | add shidy dckr

--- branch-lib/components/editor.tsx
+++ branch-lib/components/editor.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import type QuillType from "quill";
 import "quill/dist/quill.snow.css";
 
 export default function RichTextEditor() {
-  const editorRef = useRef(null);
-  const quillRef = useRef(null);
+  const editorRef = useRef<HTMLDivElement | null>(null);
+  const quillRef = useRef<QuillType | null>(null);
+
   const [value, setValue] = useState("");
 
   useEffect(() => {
     if (quillRef.current) return;
 
     async function loadQuill() {
-      const Quill = (await import("quill")).default;
+      const Quill = (await import("quill")).default as typeof QuillType;
+
+      if (!editorRef.current) return;
 
       quillRef.current = new Quill(editorRef.current, {
         theme: "snow",
@@ -26,7 +30,7 @@ export default function RichTextEditor() {
       });
 
       quillRef.current.on("text-change", () => {
-        setValue(quillRef.current.root.innerHTML);
+        setValue(quillRef.current!.root.innerHTML);
       });
     }
 
